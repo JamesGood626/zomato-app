b67139ac95ebdd37650fc807f18ac6125f2e3f37
James <jamesgood626@gmail.com>
2018-08-27 14:50:30 -0700

    first commit to heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const app = require("./app");
+const path = require("path");
 const graphQLServer = require("./Middleware/graphQLServer");
 
-const server = app.listen({ port: 4000 }, () =>
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const port = process.env.PORT || 4000;
+
+const server = app.listen({ port }, () =>
   console.log(
     `🚀 Server ready at http://localhost:4000${graphQLServer.graphqlPath}`
   )
