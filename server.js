68fd1e0d12711262f09f13c1292d2044b6dbdaae
James <jamesgood626@gmail.com>
2018-08-27 15:54:00 -0700

    Didn't build the react app last go round

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 const server = app.listen({ port }, () =>
   console.log(
-    `🚀 Server ready at http://localhost:4000${graphQLServer.graphqlPath}`
+    `🚀 Server ready at http://localhost:5000${graphQLServer.graphqlPath}`
   )
 );
 
